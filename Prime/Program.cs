38ae4646e86cecc419371f3fef26862775637a59
Lukasz <kuxi69@gmail.com>
2016-06-27 08:23:44 -0800

    Dodanie funkcji obliczajacej liczby pierwsze

--- Prime/Program.cs
+++ Prime/Program.cs
@@ -6,11 +6,47 @@ using System.Threading.Tasks;
 
 namespace Prime
 {
-    class Program
+   public class Program
     {
+        public static void Semiprime(int M)
+        {
+            bool isPrime = true;
+            List<int> Lista = new List<int>();
+            for (int i = 2; i <= M; i++)
+            {
+
+                for (int j = 2; j <= M; j++)
+                {
+                    if (i != j && i % j == 0)
+                    {
+                        isPrime = false;
+                      
+                    }
+                }
+                if (isPrime)
+                {                   
+                    Lista.Add(i);
+                    
+                }
+                isPrime = true;
+                
+            }
+            Console.WriteLine(string.Join(",", Lista));
+            
+            Console.Write(r);
+
+        }
+
+
+
         static void Main(string[] args)
         {
-            test
+            int N = 1;
+            int M = 26;
+            int[] P={ 1, 4, 16 };
+            int[] Q={ 26, 10, 20  };
+            Semiprime(M);
+            Console.ReadKey();
         }
     }
 }
