d9e29ec7ff2666d6c694e75d4c6d8e480e7f45aa
Lukasz <kuxi69@gmail.com>
2016-06-27 09:50:20 -0800

    Dodanie sprawdzania ile licz z zakresu jest polpierwszych

--- Prime/Program.cs
+++ Prime/Program.cs
@@ -8,7 +8,7 @@ namespace Prime
 {
    public class Program
     {
-        public static void Semiprime(int M)
+        public static List<int> Semiprime(int M)
         {
             bool isPrime = true;
             List<int> Lista = new List<int>();
@@ -35,14 +35,12 @@ namespace Prime
             }
 
 
-
-
             for (int i = 0; i < Lista.Count; i++)
             {
                 for (int j = 0; j < Lista.Count(); j++)
                 {
                     int semiPrime = Lista[i] * Lista[j];
-                    if (semiPrime >= M)
+                    if (semiPrime > M)
                     {
                         break;
                     }
@@ -55,18 +53,50 @@ namespace Prime
             SemiPrimes = SemiPrimes.Distinct().ToList();
             SemiPrimes.Sort();
             Console.WriteLine(string.Join(",", SemiPrimes));
-
+            return SemiPrimes;
         }
 
+        public static void IleLiczb(int M, int[] P,int []Q)
+        {
+            List<int> Lista = new List<int>();
+            List<int> L = new List<int>();
+            List<int> Wynik = new List<int>();
+            Lista = Semiprime(M);
+            int Counter;
 
+            for (int i = 0; i < P.Length; i++)
+            {
+                Counter = 0;
+
+                if (P[i]<Q[i])
+                {
+                    for (int j = 0; j < Lista.Count; j++)
+                    {
+                        if (Lista[j] <= Q[i] & Lista[j] >= P[i])
+                        {
+                            L.Add(Lista[j]);
+                            Counter= L.Count;                                                        
+                        }
+                       
+                    }
+
+                    L.Clear();
+                   
+                   
+                }
+                Wynik.Add(Counter);
+               
+            }
+            Console.WriteLine(string.Join(",", Wynik));
+        }
 
         static void Main(string[] args)
         {
-            int N = 1;
+         
             int M = 26;
-            int[] P={ 1, 4, 16 };
-            int[] Q={ 26, 10, 20  };
-            Semiprime(M);
+            int[] P={ 1,4,16 };
+            int[] Q={26,10,20  };
+            IleLiczb(M, P, Q);
             Console.ReadKey();
         }
     }
